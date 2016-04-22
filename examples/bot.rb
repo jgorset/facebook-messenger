0446afd3ef2ec48106a2a380e695c964eeec9f1b
Johannes Gorset <jgorset@gmail.com>
2016-04-23 00:36:58 +0200

    `configure` to configure, not `config`

--- examples/bot.rb
+++ examples/bot.rb
@@ -1,6 +1,6 @@
 require 'facebook/messenger'
 
-Facebook::Messenger.config do |config|
+Facebook::Messenger.configure do |config|
   config.access_token = ENV['ACCESS_TOKEN']
   config.verify_token = ENV['VERIFY_TOKEN']
 end
