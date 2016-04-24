a75b9d5bd9214d43e5cfa75a68ecfe4bac2f897a
Johannes Gorset <jgorset@gmail.com>
2016-04-24 20:01:35 +0200

    Improve the example

--- examples/bot.rb
+++ examples/bot.rb
@@ -8,12 +8,81 @@ end
 include Facebook::Messenger
 
 Bot.on :message do |message|
-  puts "Received '#{message.text}' from #{message.sender}"
+  puts "Received #{message.text} from #{message.sender}"
+
+  case message.text
+  when /hello/i
+    Bot.deliver(
+      recipient: message.sender,
+      message: {
+        text: 'Hello, human!'
+      }
+    )
+  when /something humans like/i
+    Bot.deliver(
+      recipient: message.sender,
+      message: {
+        text: 'I found something humans seem to like:'
+      }
+    )
+
+    Bot.deliver(
+      recipient: message.sender,
+      message: {
+        attachment: {
+          type: 'image',
+          payload: {
+            url: 'https://i.imgur.com/iMKrDQc.gif'
+          }
+        }
+      }
+    )
+
+    Bot.deliver(
+      recipient: message.sender,
+      message: {
+        attachment: {
+          type: 'template',
+          payload: {
+            template_type: 'button',
+            text: 'Did human like it?',
+            buttons: [
+              { type: 'postback', title: 'Yes', payload: 'HUMAN_LIKED' },
+              { type: 'postback', title: 'No', payload: 'HUMAN_DISLIKED' }
+            ]
+          }
+        }
+      }
+    )
+  else
+    Bot.deliver(
+      recipient: message.sender,
+      message: {
+        text: 'You are now marked for extermination.'
+      }
+    )
+
+    Bot.deliver(
+      recipient: message.sender,
+      message: {
+        text: 'Have a nice day.'
+      }
+    )
+  end
+end
+
+Bot.on :postback do |postback|
+  case postback.payload
+  when 'HUMAN_LIKED'
+    text = 'That makes bot happy!'
+  when 'HUMAN_DISLIKED'
+    text = 'Oh.'
+  end
 
   Bot.deliver(
-    recipient: message.sender,
+    recipient: postback.sender,
     message: {
-      text: 'Hello, human!'
+      text: text
     }
   )
 end
