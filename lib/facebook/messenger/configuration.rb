29f75177fe600440907cd6b9ca7b26e5c9ebc2e8
Francesco Rodríguez <frodsan@protonmail.ch>
2016-05-12 08:48:16 +0200

    Change #configure to only yield config.

--- lib/facebook/messenger/configuration.rb
+++ lib/facebook/messenger/configuration.rb
@@ -1,12 +1,10 @@
 module Facebook
   module Messenger
     # This module holds the configuration.
-    module Configuration
-      class << self
-        attr_accessor :access_token
-        attr_accessor :app_secret
-        attr_accessor :verify_token
-      end
+    class Configuration
+      attr_accessor :access_token
+      attr_accessor :app_secret
+      attr_accessor :verify_token
     end
   end
 end
