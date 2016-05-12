ef0b534f33a4cbc52f363db3e74e32c5a35f72f5
Francesco Rodríguez <frodsan@protonmail.ch>
2016-05-12 09:16:13 +0200

    Run specs in a blank slate.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,11 @@ require 'helpers/graph_api_helpers'
 Coveralls.wear!
 
 require 'facebook/messenger'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    Facebook::Messenger.config = Facebook::Messenger::Configuration.new
+
+    Facebook::Messenger::Bot.unhook
+  end
+end
