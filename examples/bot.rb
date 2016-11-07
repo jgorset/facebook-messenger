37aa0bb01c1c4b20182a0d88e126f98d566f38e4
Johannes Gorset <jgorset@gmail.com>
2016-11-07 19:48:42 +0100

    Change example to use configuration provider

--- examples/bot.rb
+++ examples/bot.rb
@@ -1,10 +1,5 @@
 require 'facebook/messenger'
 
-Facebook::Messenger.configure do |config|
-  config.access_token = ENV['ACCESS_TOKEN']
-  config.verify_token = ENV['VERIFY_TOKEN']
-end
-
 include Facebook::Messenger
 
 Bot.on :message do |message|
