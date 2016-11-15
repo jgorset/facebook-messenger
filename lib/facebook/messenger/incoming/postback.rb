6c533e7aa12124156613e42eeaf5e43c45454104
Borja Martin <borja@traity.com>
2016-11-15 22:02:19 +0100

    Add support for referral params

--- lib/facebook/messenger/incoming/postback.rb
+++ lib/facebook/messenger/incoming/postback.rb
@@ -4,6 +4,12 @@ module Facebook
       # The Postback class represents an incoming Facebook Messenger postback.
       class Postback
         include Facebook::Messenger::Incoming::Common
+        attr_reader :referral_param
+
+        def initialize(messaging)
+          super
+          @referral_param ||= Facebook::Messenger::Incoming::Referral::Param.new(messaging['postback']['referral']) if messaging['postback']['referral']
+        end
 
         def payload
           @messaging['postback']['payload']
