710fd7f8a883d745ecf137b41318f19278c93699
Ulrich Sossou <sorich87@gmail.com>
2016-12-15 10:37:01 +0100

    Improve handling of referral in postback

--- lib/facebook/messenger/incoming/postback.rb
+++ lib/facebook/messenger/incoming/postback.rb
@@ -4,20 +4,15 @@ module Facebook
       # The Postback class represents an incoming Facebook Messenger postback.
       class Postback
         include Facebook::Messenger::Incoming::Common
-        attr_reader :referral
-
-        def initialize(messaging)
-          super
-
-          if messaging['postback'] && messaging['postback']['referral']
-            @referral = Referral::Referral.new(
-              messaging['postback']['referral']
-            )
-          end
-        end
 
         def payload
           @messaging['postback']['payload']
+        end
+
+        def referral
+          @referral ||= Referral::Referral.new(
+            @messaging['postback']['referral']
+          )
         end
       end
     end
