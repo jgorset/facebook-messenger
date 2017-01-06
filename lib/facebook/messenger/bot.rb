f2a8e4af3d228b13915c2b004c44445046262d07
Jim Neath <jimneath@googlemail.com>
2017-01-06 19:29:19 +0000

    hardcode page ids because im in a rush

--- lib/facebook/messenger/bot.rb
+++ lib/facebook/messenger/bot.rb
@@ -4,8 +4,13 @@ module Facebook
     module Bot
       include HTTParty
 
-      base_uri 'https://graph.facebook.com/v2.6/me'
-
+      # lazines
+      if Rails.env.production?
+        base_uri 'https://graph.facebook.com/v2.8/386624931385753'
+      else 
+        base_uri 'https://graph.facebook.com/v2.8/230591880697484'
+      end
+      
       EVENTS = [:message, :delivery, :postback, :optin,
                 :read, :account_linking].freeze
 
