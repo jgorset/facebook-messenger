b41b39dee8b73b4c056346267504243e75c4dc86
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2017-11-29 17:20:56 -0500

    fix rubocop errors introduced in #186

--- lib/facebook/messenger/bot/message_type.rb
+++ lib/facebook/messenger/bot/message_type.rb
@@ -3,10 +3,10 @@ module Facebook
     module Bot
       # Supported message types
       module MessageType
-        RESPONSE = "RESPONSE".freeze
-        UPDATE = "UPDATE".freeze
-        MESSAGE_TAG = "MESSAGE_TAG".freeze
-        NON_PROMOTIONAL_SUBSCRIPTION = "NON_PROMOTIONAL_SUBSCRIPTION".freeze
+        RESPONSE = 'RESPONSE'.freeze
+        UPDATE = 'UPDATE'.freeze
+        MESSAGE_TAG = 'MESSAGE_TAG'.freeze
+        NON_PROMOTIONAL_SUBSCRIPTION = 'NON_PROMOTIONAL_SUBSCRIPTION'.freeze
       end
     end
   end
